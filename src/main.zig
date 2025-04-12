10db9e0a169f37ef1ea0febb4e59093867e4b927
Wojciech Chuchro <82377967+WojciechChuchro@users.noreply.github.com>
2025-04-12 22:15:59 +0200

    feat: implement base 64 algorithm

--- src/main.zig
+++ src/main.zig
@@ -16,6 +16,20 @@ pub const Base64 = struct {
         return self.table[index];
     }
 
+    fn char_index(self: Base64, char: u8) u8 {
+        if (char == '=')
+            return 64;
+
+        var index: u8 = 0;
+        for (0..63) |_| {
+            if (self.char_at(index) == char)
+                break;
+            index += 1;
+        }
+
+        return index;
+    }
+
     fn calculate_output_length(input: []const u8) !usize {
         if (input.len < 3) {
             return 4;
@@ -47,34 +61,65 @@ pub const Base64 = struct {
         const output = try allocator.alloc(u8, try calculate_output_length(input));
         var count: u8 = 0;
         var tmp = [3]u8{ 0, 0, 0 };
+        var idx: u8 = 0;
 
         for (input, 0..) |_, i| {
             tmp[count] = input[i];
             count += 1;
 
             if (count == 3) {
-                output[0] = self.char_at(tmp[0] >> 2);
-                output[1] = self.char_at(((tmp[0] & 0x03) << 4) + (tmp[1] >> 4));
-                output[2] = self.char_at(((tmp[1] & 0x0f) << 2) + (tmp[2] >> 6));
-                output[3] = self.char_at(tmp[2] & 0x3f);
+                output[idx] = self.char_at(tmp[0] >> 2);
+                output[idx + 1] = self.char_at(((tmp[0] & 0x03) << 4) + (tmp[1] >> 4));
+                output[idx + 2] = self.char_at(((tmp[1] & 0x0f) << 2) + (tmp[2] >> 6));
+                output[idx + 3] = self.char_at(tmp[2] & 0x3f);
                 count = 0;
+                idx += 4;
             }
 
             if (count == 2) {
-                // TODO:
+                output[idx] = self.char_at(tmp[0] >> 2);
+                output[idx + 1] = self.char_at(((tmp[0] & 0x03) << 4) + (tmp[1] >> 4));
+                output[idx + 2] = self.char_at(((tmp[1] & 0x0f) << 2) + (tmp[2] >> 6));
+                output[idx + 3] = '=';
             }
+
             if (count == 1) {
-                // TODO:
+                output[idx] = self.char_at(tmp[0] >> 2);
+                output[idx + 1] = self.char_at(((tmp[0] & 0x03) << 4) + (tmp[1] >> 4));
+                output[idx + 2] = '=';
+                output[idx + 3] = '=';
             }
         }
 
-        std.debug.print("base64: {s}\n", .{output});
         return output;
     }
 
-    fn decode(self: Base64, allocator: std.mem.Allocator, input: []const u8) []const u8 {
-        const decoded = try allocator.alloc(u8, calculate_decode_length(input));
-        _ = self;
+    fn decode(self: Base64, allocator: std.mem.Allocator, input: []const u8) ![]u8 {
+        var decoded = try allocator.alloc(u8, try calculate_decode_length(input));
+        var count: u8 = 0;
+        var tmp = [4]u8{ 0, 0, 0, 0 };
+        var idx: u8 = 0;
+
+        for (0..input.len) |i| {
+            tmp[count] = self.char_index(input[i]);
+            count += 1;
+
+            if (count == 4) {
+                decoded[idx] = (tmp[0] << 2) + (tmp[1] >> 4);
+
+                if (tmp[2] != 64) {
+                    decoded[idx + 1] = (tmp[1] << 4) + (tmp[2] >> 2);
+                }
+
+                if (tmp[3] != 64) {
+                    decoded[idx + 2] = (tmp[2] << 6) + tmp[3];
+                }
+
+                count = 0;
+                idx += 3;
+            }
+        }
+
         return decoded;
     }
 };
@@ -84,10 +129,15 @@ pub fn main() !void {
     var buffer: [1000]u8 = undefined;
     var fba = std.heap.FixedBufferAllocator.init(&buffer);
     const alloc = fba.allocator();
-    const material = "elo"; // base64: aGkgaXRzIG1l
+    const input = "Hello world";
 
-    const encoded = try base64.encode(alloc, material);
+    const encoded = try base64.encode(alloc, input);
     defer alloc.free(encoded);
 
-    std.debug.print("encodedc: {s}\n", .{encoded});
+    const decoded = try base64.decode(alloc, encoded);
+    defer alloc.free(decoded);
+
+    std.debug.print("input: {s}\n", .{input});
+    std.debug.print("encoded: {s}\n", .{encoded});
+    std.debug.print("decoded: {s}\n", .{decoded});
 }
